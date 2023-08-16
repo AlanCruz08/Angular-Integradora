bdbd01b6d7f83feb26a6b86aaba4753dffb149fd
JoseEscobar128 <guillermoescobar128@gmail.com>
2023-08-16 14:13:56 -0600

    cambios en el diseño de alcohol

--- src/app/components/sensores/alcohol/alcohol.component.ts
+++ src/app/components/sensores/alcohol/alcohol.component.ts
@@ -9,14 +9,14 @@ import { SensoresAll } from 'src/app/interface/sensores';
   styleUrls: ['./alcohol.component.css']
 })
 export class AlcoholComponent implements OnInit {
-  valoresAlcohol: SensoresAll[] = [];
-  valoresFiltrados: SensoresAll[] = [];
-  fechaBusqueda: string = '';
+  valoresAlcohol: SensoresAll[] = [];        // Arreglo para almacenar los valores originales
+  valoresFiltrados: SensoresAll[] = [];      // Arreglo para almacenar los valores filtrados
+  fechaBusqueda: string = '';                // Variable para almacenar la fecha de búsqueda
 
   constructor(private router: Router, private secureService: SecureService) { }
 
   ngOnInit(): void {
-    this.obtenerValoresAlcohol();
+    this.obtenerValoresAlcohol();            // Al inicializar el componente, se obtienen los valores
   }
 
   obtenerValoresAlcohol(): void {
@@ -25,10 +25,10 @@ export class AlcoholComponent implements OnInit {
         console.log('Respuesta del servidor:', response);
 
         if (Array.isArray(response.data)) {
-          this.valoresAlcohol = response.data;
+          this.valoresAlcohol = response.data;   // Almacenar valores de alcohol en el arreglo
           console.log('Valores de alcohol asignados:', this.valoresAlcohol);
         } else if (response.data && typeof response.data === 'object') {
-          this.valoresAlcohol = [response.data];
+          this.valoresAlcohol = [response.data];  // Convertir objeto individual en un arreglo
           console.log('Valor de alcohol individual asignado:', this.valoresAlcohol);
         } else {
           console.error('Los datos de alcohol no son válidos:', response.data);
@@ -43,20 +43,23 @@ export class AlcoholComponent implements OnInit {
   buscarPorFecha(): void {
     if (this.fechaBusqueda) {
       console.log('Fecha de búsqueda:', this.fechaBusqueda);
-      
+
+      // Convertir la fecha de búsqueda al formato adecuado
       const fechaBusquedaFormatted = this.formatoFechaBusqueda(this.fechaBusqueda);
-      
+
+      // Filtrar los valores por la fecha de búsqueda
       this.valoresFiltrados = this.valoresAlcohol.filter((valor) => {
         console.log('Comparando fechas:', valor.fecha, fechaBusquedaFormatted);
         return this.sonFechasIguales(valor.fecha, fechaBusquedaFormatted);
       });
-      
+
       console.log('Valores filtrados:', this.valoresFiltrados);
     } else {
-      this.valoresFiltrados = [];
+      this.valoresFiltrados = [];   // Si no hay fecha de búsqueda, vaciar los valores filtrados
     }
   }
-  
+
+  // Función para convertir la fecha de búsqueda al formato adecuado
   formatoFechaBusqueda(fecha: string): string {
     const dateParts = fecha.split('-');
     const day = dateParts[2];
@@ -64,8 +67,8 @@ export class AlcoholComponent implements OnInit {
     const year = dateParts[0];
     return `${day}/${month}/${year}`;
   }
-  
 
+  // Función para comparar si dos fechas son iguales (solo la parte de la fecha, sin hora)
   sonFechasIguales(fecha1: string, fecha2: string): boolean {
     return fecha1.split(' ')[0] === fecha2;
   }
