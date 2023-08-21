c1bd1809a6beb53660d3e279408ac8812cbb3da4
JoseEscobar128 <guillermoescobar128@gmail.com>
2023-08-21 03:08:03 -0600

    feat- temperatura/pir - se agrego la paginacion y un mensaje si no hay datos en la tabla

--- src/app/components/sensores/pir/pir.component.ts
+++ src/app/components/sensores/pir/pir.component.ts
@@ -15,36 +15,52 @@ export class PirComponent implements OnInit {
   fechaInicial: string = ''; // Fecha inicial del rango de búsqueda
   fechaFinal: string = ''; // Fecha final del rango de búsqueda
 
-  constructor(private router: Router, private secureService: SecureService) { }
+  currentPage: number = 1; // Página actual
+  itemsPerPage: number = 20; // Cantidad de elementos por página
+  totalPages: number = 0;
 
+  constructor(private router: Router, private secureService: SecureService) { }
+  changePage(step: number): void {
+    const newPage = this.currentPage + step;
+    if (newPage >= 1 && newPage <= this.totalPages) {
+      this.currentPage = newPage;
+    }
+  }
   ngOnInit(): void {
-    this.obtenerValoresPir();
+    this.obtenerValoresPir(); // Llama a la función para obtener todos los valores de temperatura
+  }
+
+  get valoresPaginados(): SensoresAll[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.fechaBusqueda ? this.valoresFiltrados.slice(startIndex, endIndex) : this.valoresPir.slice(startIndex, endIndex);
   }
 
   obtenerValoresPir(): void {
     this.secureService.getPIRAll().subscribe(
       (response: any) => {
         console.log('Respuesta del servidor:', response);
-
         if (Array.isArray(response.data)) {
-          this.valoresPir = response.data;
-          console.log('Valores de pir asignados:', this.valoresPir);
+          this.valoresPir = response.data; // Asigna los valores de temperatura recuperados
+          console.log('Valores de movimiento asignados:', this.valoresPir);
+          this.totalPages = Math.ceil(this.valoresPir.length / this.itemsPerPage);
+          this.currentPage = 1;
         } else if (response.data && typeof response.data === 'object') {
-          // Si es un objeto individual, crea un array con ese objeto
-          this.valoresPir = [response.data];
-          console.log('Valor de pir individual asignado:', this.valoresPir);
+          this.valoresPir = [response.data]; // Crea un array con el valor de temperatura individual
+          console.log('Valor de movimiento individual asignado:', this.valoresPir);
         } else {
-          console.error('Los datos de pir no son válidos:', response.data);
+          console.error('Los datos de movimiento no son válidos:', response.data);
         }
       },
       (error: any) => {
-        console.error('Error al obtener los valores de pir:', error);
+        console.error('Error al obtener los valores de movimiento:', error);
       }
     );
   }
+
   buscarValores(): void {
     const filtro = {
-      sensor_id: 4, // Cambia este valor al ID del sensor que desees
+      sensor_id: 1, // Cambia este valor al ID del sensor que desees
       fecha_inicial: this.fechaInicial,
       fecha_final: this.fechaFinal
     };
@@ -58,7 +74,9 @@ export class PirComponent implements OnInit {
         if (Array.isArray(response.data)) {
           this.valoresFiltrados = response.data; // Asigna los valores filtrados a la variable
           console.log('Valores filtrados asignados:', this.valoresFiltrados);
-          this.fechaBusqueda = true; // Marca que hay una búsqueda activa
+          this.totalPages = Math.ceil(this.valoresFiltrados.length / this.itemsPerPage);
+          this.currentPage = 1;
+          this.fechaBusqueda = true;
         } else {
           console.error('Los datos filtrados no son válidos:', response.data);
           this.fechaBusqueda = false; // Marca que no hay una búsqueda activa
